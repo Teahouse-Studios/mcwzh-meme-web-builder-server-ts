d7686e5c1a085fa26cf12f09892f02f99cc05b9d
LittleC <i@ltlec.cn>
2021-09-04 19:51:09 +0800

    :sparkles: build api

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import Koa from 'koa'
 import Router from '@koa/router'
+import { tmpdir } from 'os'
 import { resolve } from 'path'
 import { readdir } from 'fs/promises'
 import { MemepackBuilder } from 'memepack-builder'
 import cors from '@koa/cors'
+import koaBody from 'koa-body'
 
 const app = new Koa()
 const router = new Router()
 app.use(cors())
+app.use(koaBody())
 
 const jePath = resolve(__dirname, '..', 'data', 'mcwzh-meme-resourcepack')
 const bePath = resolve(__dirname, '..', 'data', 'mcwzh-meme-resourcepack-bedrock')
@@ -24,6 +27,20 @@ router.get('/', async (ctx) => {
     be_modules: be.moduleChecker.moduleInfo().modules,
     je_modified: 0,
     be_modified: 0
+  }
+})
+
+router.post('/ajax', async (ctx) => {
+  const {type, modules, mod, sfw, format} = ctx.request.body
+  je.builder.options = {
+    type, modules, mod, sfw, format, hash: true,
+    outputDir: tmpdir()
+  }
+  let r = await je.build(true)
+  ctx.body = {
+    code: 200,
+    logs: je.log.join('\n'),
+    filename: r.name
   }
 })
 
