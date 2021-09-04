b64657a0793166f0c5c8d8691c59297390536469
LittleC <i@ltlec.cn>
2021-09-04 21:49:23 +0800

    :sparkles: build and deploy

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Koa from 'koa'
 import Router from '@koa/router'
 import { tmpdir } from 'os'
 import { resolve } from 'path'
-import { readdir } from 'fs/promises'
+import { readdirSync } from 'fs'
 import { MemepackBuilder } from 'memepack-builder'
 import cors from '@koa/cors'
 import koaBody from 'koa-body'
@@ -12,21 +12,31 @@ const router = new Router()
 app.use(cors())
 app.use(koaBody())
 
-const jePath = resolve(__dirname, '..', 'data', 'mcwzh-meme-resourcepack')
-const bePath = resolve(__dirname, '..', 'data', 'mcwzh-meme-resourcepack-bedrock')
+
+const root = process.env.NODE_ENV === 'production' ? '/mnt/meme/' : resolve(__dirname, '..', 'data')
+
+const jePath = resolve(root, 'mcwzh-meme-resourcepack')
+const bePath = resolve(root, 'mcwzh - meme - resourcepack - bedrock')
 
 const je = new MemepackBuilder('je', resolve(jePath, 'meme_resourcepack'), resolve(jePath, 'modules'))
 const be = new MemepackBuilder('be', resolve(bePath, 'meme_resourcepack'), resolve(jePath, 'modules'))
 
 router.get('/', async (ctx) => {
-  let mods = (await readdir(resolve(jePath, 'mods'))).map(v => `mods/${v}`)
-  let enmods = (await readdir(resolve(jePath, 'en-mods'))).map(v => `en-mods/${v}`)
-  ctx.body = {
-    mods, enmods,
-    je_modules: je.moduleChecker.moduleInfo().modules,
-    be_modules: be.moduleChecker.moduleInfo().modules,
-    je_modified: 0,
-    be_modified: 0
+  try {
+    let mods = (await readdirSync(resolve(jePath, 'mods'))).map(v => `mods/${v}`)
+    let enmods = (await readdirSync(resolve(jePath, 'en-mods'))).map(v => `en-mods/${v}`)
+    ctx.body = {
+      mods, enmods,
+      je_modules: je.moduleChecker.moduleInfo().modules,
+      be_modules: be.moduleChecker.moduleInfo().modules,
+      je_modified: 0,
+      be_modified: 0
+    }
+  } catch (e) {
+    ctx.status = 403
+    ctx.body = {
+      message: e.stack
+    }
   }
 })
 
@@ -56,4 +66,6 @@ router.post('/ajax', async (ctx) => {
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-app.listen(8000)
+const server = app.listen(9000)
+server.timeout = 0
+server.keepAliveTimeout = 0
